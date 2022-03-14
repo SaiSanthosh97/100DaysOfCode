8d52d50c776b6f08468332d62ba86187c4e066bd
SaiSanthosh97 <saisanthosh.facebook@gmail.com>
2022-03-14 12:19:40 +0530

    Base version of Singleton

---
+++ Singleton Pattern/SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Singleton s1 = Singleton.GetInstance;
+            s1.PrintDetails("Hello");
+            Singleton s2 = Singleton.GetInstance;
+            s2.PrintDetails("World");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton Pattern/SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+
+    //This code works when single thread is trying to create instance of a class
+    //This is not enough to handle multiple thread
+
+    //Making this as sealed so it cannot be inherited
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        //As the class is sealed we need to provide an alternate way to give back the singleton object creation
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+        //Using private constructor to avoid multiple instance creation of a class
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
