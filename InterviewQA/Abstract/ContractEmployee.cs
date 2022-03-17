d7ab23801100d67ff8e66edd81c6d0716e61975b
SaiSanthosh97 <saisanthosh.facebook@gmail.com>
2022-03-17 22:27:31 +0530

    BaseEmployee

--- InterviewQA/Abstract/ContractEmployee.cs
+++ InterviewQA/Abstract/ContractEmployee.cs
@@ -8,16 +8,8 @@ namespace Abstract
 {
     class ContractEmployee
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public int HourlyPay { get; set; }
         public int TotalHoursWorked { get; set; }
-
-        public string GetFullName()
-        {
-            return this.FirstName + " " + this.LastName;
-        }
 
         public int GetMonthlySalary()
         {
--- InterviewQA/Abstract/FullTimeEmployee.cs
+++ InterviewQA/Abstract/FullTimeEmployee.cs
@@ -8,15 +8,8 @@ namespace Abstract
 {
     class FullTimeEmployee
     {
-        public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public int AnnualSalary { get; set; }
 
-        public string GetFullName()
-        {
-            return this.FirstName + " " + this.LastName;
-        }
         public int GetMonthlySalary()
         {
             return this.AnnualSalary / 12;
