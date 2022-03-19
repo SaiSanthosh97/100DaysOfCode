6f5bf25874b7a51d5a84f21aa398b0ec9083520c
SaiSanthosh97 <saisanthosh.facebook@gmail.com>
2022-03-19 14:59:11 +0530

    Interface

---
+++ InterviewQA/Interface/Explicit.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+class Program : I1, I2
+{
+    static void Main()
+    {
+        Program P = new Program();
+        ((I1)P).InterfaceMethod();
+        ((I2)P).InterfaceMethod();
+    }
+    void I1.InterfaceMethod()
+    {
+        Console.WriteLine("I1 interface method implemented");
+    }
+    void I2.InterfaceMethod()
+    {
+        Console.WriteLine("I2 interface method implemented");
+    }
+}
+
+interface I1
+{
+    void InterfaceMethod();
+}
+interface I2
+{
+    void InterfaceMethod();
+}
